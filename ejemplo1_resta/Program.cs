25bbae0f88f59f407b9018f560604b4e1cb776b7
wilbert11 <wilberth11@gmail.com>
2014-10-21 10:44:55 -0600

    Eliminacion de codigo

--- ejemplo1_resta/Program.cs
+++ ejemplo1_resta/Program.cs
@@ -10,14 +10,7 @@ namespace ejemplo1_resta
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Dame el primer número");
-            int numero1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Dame el segundo número");
-            int numero2 = int.Parse(Console.ReadLine());
-
-            int resultado = numero1 - numero2;
-            Console.WriteLine("El resultado es: " + resultado);
-            Console.ReadLine();
+           
         }
     }
 }
